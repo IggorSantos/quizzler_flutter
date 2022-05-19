58e3c5227bdf827561348aee1bbbcb513af92388
Igor Santos <igor.sagitarius@gmail.com>
2022-05-18 21:06:52 -0300

    Inclusão das respostas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -31,6 +32,12 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.'
   ];
+  List <bool> answers = [
+    false,
+    true,
+    true
+  ];
+  Question q1 = Question(questionText = 'You can lead a cow down stairs but not up stairs.', questionAnswer = false);
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -68,6 +75,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                if(answers[questionNumber] == true){
+                  print("Acertou");
+                }else{
+                  print("Errou");
+                }
                 setState(() {
                   questionNumber ++;
                 });
@@ -88,6 +100,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+              if(answers[questionNumber] == false){
+                print("Acertou");
+              }else{
+                print("Errou");
+              }
               setState(() {
                 questionNumber ++;
               });
